f59892a2ecf0f6b227bf7ed4f12b5b02fcfdb05b
Marko Kosunen <marko.kosunen@aalto.fi>
2018-04-28 00:16:53 -0700

    Prog delays added to f2_rx_path and_f2_rx_dsp

--- src/main/scala/f2_rx_path/f2_rx_path.scala
+++ src/main/scala/f2_rx_path/f2_rx_path.scala
@@ -9,24 +9,37 @@ import dsptools._
 import dsptools.numbers._
 import freechips.rocketchip.util._
 import f2_decimator._
+import prog_delay._
 
 
-class f2_rx_path_io(val inputn: Int=9, val n: Int=16,  val users: Int=4 ) extends Bundle {
+// How to extract the "resolution" of a type?
+class f2_rx_path_io (
+        val inputn   : Int=9, 
+        val n        : Int=16,  
+        val users    : Int=4,
+        val progdelay: Int=64
+
+    ) extends Bundle {
     val decimator_clocks   = new f2_decimator_clocks()
     val decimator_controls = new f2_decimator_controls(gainbits=10)
     val adc_clock          = Input(Clock())
     val reset_adcfifo      = Input(Bool())
     val adc_fifo_lut_mode  = Input(UInt(3.W))
     val adc_lut_write_addr = Input(UInt(inputn.W))
-    val adc_lut_write_val = Input(DspComplex(SInt(inputn.W), SInt(inputn.W)))
+    val adc_lut_write_val  = Input(DspComplex(SInt(inputn.W), SInt(inputn.W)))
     val adc_lut_write_en   = Input(Bool())
-    val iptr_A      = Input(DspComplex(SInt(inputn.W), SInt(inputn.W)))
-    val Z           = Output(Vec(users,DspComplex(SInt(n.W), SInt(n.W))))
+    val user_delays        = Input(Vec(users,UInt(log2Ceil(progdelay).W)))
+    val iptr_A             = Input(DspComplex(SInt(inputn.W), SInt(inputn.W)))
+    val Z                  = Output(Vec(users,DspComplex(SInt(n.W), SInt(n.W))))
 }
 
-class f2_rx_path (inputn: Int=9, n: Int=16, users: Int=4) extends Module {
-    val io = IO( new f2_rx_path_io(inputn=inputn,users=users)
-    )
+class f2_rx_path (
+        inputn: Int=9, 
+        n: Int=16, 
+        users: Int=4,
+        progdelay: Int=64
+    ) extends Module {
+    val io = IO( new f2_rx_path_io(inputn=inputn,users=users,progdelay=progdelay))
   
     val decimator  = Module ( new  f2_decimator (n=n, resolution=32, coeffres=16, gainbits=10)).io
     io.decimator_controls<>decimator.controls
@@ -87,8 +100,6 @@ class f2_rx_path (inputn: Int=9, n: Int=16, users: Int=4) extends Module {
     when (io.adc_lut_write_en===true.B) {
         adclut_real.write(io.adc_lut_write_addr,io.adc_lut_write_val.real)
         adclut_imag.write(io.adc_lut_write_addr,io.adc_lut_write_val.imag)
-        //w_lutoutdata.real:=adclut_real.read(w_lutreadaddress.real.asUInt)
-        //w_lutoutdata.imag:=adclut_real.read(w_lutreadaddress.imag.asUInt)
     } 
     .otherwise {
         w_lutoutdata.real:=adclut_real.read(w_lutreadaddress.real.asUInt)
@@ -96,7 +107,27 @@ class f2_rx_path (inputn: Int=9, n: Int=16, users: Int=4) extends Module {
     }
     //RX input assignments        
     decimator.iptr_A:=w_inselect
-    io.Z.map(_:=decimator.Z)
+
+    //Gives a possibility to tune each user separately
+    //even they are currently only one data stream
+    val proto=DspComplex(SInt(n.W), SInt(n.W))
+    val p_delay= Seq.fill(users){ 
+        withClock(io.decimator_clocks.hb3clock_low)(
+            Module( new prog_delay(proto, maxdelay=progdelay)).io
+        )
+    }
+    //val p_delay= Module( new prog_delay(proto, maxdelay=progdelay))
+    
+    p_delay.map(_.iptr_A:=decimator.Z)
+    (p_delay,io.user_delays).zipped.map(_.select:=_)
+
+    when (io.decimator_controls.mode===0.U) {
+        io.Z.map(_:=RegNext(decimator.Z))
+    } .otherwise {
+        //These are in the same clock domain if
+        //the decimator in NOT bypassed
+        (io.Z,p_delay).zipped.map(_:=_.optr_Z)
+    }
 }
 //This gives you verilog
 object f2_rx_path extends App {
