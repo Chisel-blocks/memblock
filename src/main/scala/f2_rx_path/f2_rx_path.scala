eed53f310f25bfd51ea9cc0fc85edb9b3566c09e
Marko Kosunen <marko.kosunen@aalto.fi>
2018-04-30 20:56:20 -0700

    MAJOR: DSP modified to sync through ready and valid of the Decoupled IO
    modified:   src/main/scala/f2_dsp/f2_dsp.scala
    modified:   src/main/scala/f2_rx_dsp/f2_rx_dsp.scala
    modified:   src/main/scala/f2_rx_path/f2_rx_path.scala

--- src/main/scala/f2_rx_path/f2_rx_path.scala
+++ src/main/scala/f2_rx_path/f2_rx_path.scala
@@ -11,33 +11,47 @@ import freechips.rocketchip.util._
 import f2_decimator._
 import prog_delay._
 
+class rx_path_adc_ioctrl (
+        val inputn   : Int=9,
+        val n        : Int=16,
+        val users    : Int=4,
+        val progdelay: Int=64,
+        val finedelay: Int=32
+    ) extends Bundle {
+        val adc_fifo_lut_mode  = UInt(3.W)
+        val reset_adcfifo      = Bool()
+        val adc_lut_write_addr = UInt(inputn.W)
+        val adc_lut_write_val  = DspComplex(SInt(inputn.W), SInt(inputn.W))
+        val adc_lut_write_en   = Bool()
+        val user_delays        = Vec(users,UInt(log2Ceil(progdelay).W))
+        val fine_delays        = UInt(log2Ceil(finedelay).W)
+}
 
 // How to extract the "resolution" of a type?
 class f2_rx_path_io (
-        val inputn   : Int=9, 
-        val n        : Int=16,  
-        val users    : Int=4,
-        val progdelay: Int=64
+        val inputn    : Int=9, 
+        val n         : Int=16,  
+        val users     : Int=4,
+        val progdelay : Int=64,
+        val finedelay : Int=32
 
     ) extends Bundle {
     val decimator_clocks   = new f2_decimator_clocks()
     val decimator_controls = new f2_decimator_controls(gainbits=10)
     val adc_clock          = Input(Clock())
-    val reset_adcfifo      = Input(Bool())
-    val adc_fifo_lut_mode  = Input(UInt(3.W))
-    val adc_lut_write_addr = Input(UInt(inputn.W))
-    val adc_lut_write_val  = Input(DspComplex(SInt(inputn.W), SInt(inputn.W)))
-    val adc_lut_write_en   = Input(Bool())
-    val user_delays        = Input(Vec(users,UInt(log2Ceil(progdelay).W)))
+    val adc_ioctrl         = Input(new rx_path_adc_ioctrl(inputn=inputn, n=n,
+                                        users=users,progdelay=progdelay,
+                                        finedelay=finedelay))
     val iptr_A             = Input(DspComplex(SInt(inputn.W), SInt(inputn.W)))
     val Z                  = Output(Vec(users,DspComplex(SInt(n.W), SInt(n.W))))
 }
 
 class f2_rx_path (
         inputn: Int=9, 
-        n: Int=16, 
-        users: Int=4,
-        progdelay: Int=64
+        n         : Int=16, 
+        users     : Int=4,
+        progdelay : Int=64,
+        finedelay : Int=32
     ) extends Module {
     val io = IO( new f2_rx_path_io(inputn=inputn,users=users,progdelay=progdelay))
   
@@ -49,10 +63,16 @@ class f2_rx_path (
     val adcfifo = Module (new AsyncQueue(adcproto,depth=adcfifodepth)).io
     adcfifo.enq_clock:=io.adc_clock
     adcfifo.enq.valid:=true.B
-    adcfifo.enq_reset:=io.reset_adcfifo
-    adcfifo.deq_reset:=io.reset_adcfifo
+    adcfifo.enq_reset:=io.adc_ioctrl.reset_adcfifo
+    adcfifo.deq_reset:=io.adc_ioctrl.reset_adcfifo
     adcfifo.deq_clock:=clock
     adcfifo.deq.ready:=true.B
+
+    // Adc delay compensation
+    val adcdelay=Module( new prog_delay(adcproto, maxdelay=finedelay)).io
+    
+    adcdelay.iptr_A:=adcfifo.deq.bits
+    adcdelay.select<>io.adc_ioctrl.fine_delays
 
     //ADC lookup tables
     val adclut_real= Mem(scala.math.pow(2,9).toInt,SInt(inputn.W))
@@ -64,28 +84,28 @@ class f2_rx_path (
     //Input selection wire
     val w_inselect = Wire(DspComplex(SInt(inputn.W), SInt(inputn.W)))
 
-    when (io.adc_fifo_lut_mode===0.U) {
+    when (io.adc_ioctrl.adc_fifo_lut_mode===0.U) {
         //Bypass FIFO and LUT
         w_inselect:=io.iptr_A
         adcfifo.enq.bits:= io.iptr_A
-        w_lutreadaddress.real:= io.adc_lut_write_addr.asSInt
-        w_lutreadaddress.imag:= io.adc_lut_write_addr.asSInt
-    } .elsewhen (io.adc_fifo_lut_mode===1.U) {
+        w_lutreadaddress.real:= io.adc_ioctrl.adc_lut_write_addr.asSInt
+        w_lutreadaddress.imag:= io.adc_ioctrl.adc_lut_write_addr.asSInt
+    } .elsewhen (io.adc_ioctrl.adc_fifo_lut_mode===1.U) {
         //LUT bypassed, FIFO active
         adcfifo.enq.bits:=io.iptr_A
-        w_inselect:=adcfifo.deq.bits
-    } .elsewhen (io.adc_fifo_lut_mode===2.U) {
+        w_inselect:=adcdelay.optr_Z
+    } .elsewhen (io.adc_ioctrl.adc_fifo_lut_mode===2.U) {
        //FIFO active, LUt active
        adcfifo.enq.bits:=io.iptr_A
-       w_lutreadaddress:=adcfifo.deq.bits
+       w_lutreadaddress:=adcdelay.optr_Z
        w_inselect:=w_lutoutdata
-    } .elsewhen (io.adc_fifo_lut_mode===3.U) {
+    } .elsewhen (io.adc_ioctrl.adc_fifo_lut_mode===3.U) {
        //FIFO active, LUT active, LUT first
        //Sync problem assumed
        w_lutreadaddress:=io.iptr_A
        adcfifo.enq.bits:=w_lutoutdata
-       w_inselect:=adcfifo.deq.bits
-    } .elsewhen (io.adc_fifo_lut_mode===4.U) {
+       w_inselect:=adcdelay.optr_Z
+    } .elsewhen (io.adc_ioctrl.adc_fifo_lut_mode===4.U) {
        //LUT active, FIFO bypassed
        //Sync problem assumed
        adcfifo.enq.bits:= io.iptr_A
@@ -93,13 +113,13 @@ class f2_rx_path (
        w_inselect:=w_lutoutdata
     } .otherwise {
        adcfifo.enq.bits:= io.iptr_A
-       w_inselect:=adcfifo.deq.bits
+       w_inselect:=adcdelay.optr_Z
     }
     
     //Enabled read
-    when (io.adc_lut_write_en===true.B) {
-        adclut_real.write(io.adc_lut_write_addr,io.adc_lut_write_val.real)
-        adclut_imag.write(io.adc_lut_write_addr,io.adc_lut_write_val.imag)
+    when (io.adc_ioctrl.adc_lut_write_en===true.B) {
+        adclut_real.write(io.adc_ioctrl.adc_lut_write_addr,io.adc_ioctrl.adc_lut_write_val.real)
+        adclut_imag.write(io.adc_ioctrl.adc_lut_write_addr,io.adc_ioctrl.adc_lut_write_val.imag)
     } 
     .otherwise {
         w_lutoutdata.real:=adclut_real.read(w_lutreadaddress.real.asUInt)
@@ -110,23 +130,21 @@ class f2_rx_path (
 
     //Gives a possibility to tune each user separately
     //even they are currently only one data stream
-    val proto=DspComplex(SInt(n.W), SInt(n.W))
-    val p_delay= Seq.fill(users){ 
+    val userdelay= Seq.fill(users){ 
         withClock(io.decimator_clocks.hb3clock_low)(
-            Module( new prog_delay(proto, maxdelay=progdelay)).io
+            Module( new prog_delay(adcproto, maxdelay=progdelay)).io
         )
     }
-    //val p_delay= Module( new prog_delay(proto, maxdelay=progdelay))
     
-    p_delay.map(_.iptr_A:=decimator.Z)
-    (p_delay,io.user_delays).zipped.map(_.select:=_)
+    userdelay.map(_.iptr_A:=decimator.Z)
+    (userdelay,io.adc_ioctrl.user_delays).zipped.map(_.select:=_)
 
     when (io.decimator_controls.mode===0.U) {
         io.Z.map(_:=RegNext(decimator.Z))
     } .otherwise {
         //These are in the same clock domain if
         //the decimator in NOT bypassed
-        (io.Z,p_delay).zipped.map(_:=_.optr_Z)
+        (io.Z,userdelay).zipped.map(_:=_.optr_Z)
     }
 }
 //This gives you verilog
