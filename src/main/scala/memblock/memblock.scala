06a4e10aac9a6b129e9daddcaa1bab9e493874c2
Marko Kosunen <marko.kosunen@aalto.fi>
2018-05-06 16:14:55 -0700

    Memblock uses generic proto as type

--- src/main/scala/memblock/memblock.scala
+++ src/main/scala/memblock/memblock.scala
@@ -7,33 +7,23 @@ import chisel3.experimental._
 import dsptools._
 import dsptools.numbers._
 import f2_rx_dsp._
-class memblock ( 
-        n                : Int=16,
-        users            : Int=4,
+class memblock[T <:Data] (
+        proto            : T,
         memsize          : Int=scala.math.pow(2,13).toInt
     ) extends Module {
     val io = IO( new Bundle { 
             val write_addr = Input(UInt(log2Ceil(memsize).W))
             val read_addr  = Input(UInt(log2Ceil(memsize).W))
-            val read_val   = Output(new iofifosigs(n=n))
-            val write_val  = Input(new iofifosigs(n=n))
+            val read_val   = Output(proto)
+            val write_val  = Input(proto)
     } )
-    // Need a memory with write from scan, read to scan, and 
-    // To map this to SRAM, write address must be syncroniozed
-    // All addressing through write_addr, enables throuhg write_en
-    val userzero   = 0.U.asTypeOf(new usersigs(n=n,users=users))
-    val udatazero  = 0.U.asTypeOf(userzero.data)
-    val uindexzero = 0.U.asTypeOf(userzero.uindex)
-    val iofifozero = 0.U.asTypeOf(new iofifosigs(n=n))
-    val datazero   = 0.U.asTypeOf(iofifozero.data)
-    val rxindexzero= 0.U.asTypeOf(iofifozero.rxindex)
 
-    val mem =SyncReadMem(memsize, new iofifosigs(n=n))
+    val mem =SyncReadMem(memsize, proto)
     val write_addr =RegInit(0.U(log2Ceil(memsize).W))
     val read_addr =RegInit(0.U(log2Ceil(memsize).W))
     //val write_en =RegInit(Bool())
-    val write_val=RegInit(iofifozero)
-    val read_val =RegInit(iofifozero)
+    val write_val=RegInit(0.U.asTypeOf(io.write_val))
+    val read_val =RegInit(0.U.asTypeOf(io.read_val))
     write_addr:=io.write_addr
     write_val:=io.write_val
     read_addr:=io.read_addr
@@ -48,7 +38,8 @@ class memblock (
 }
 //This gives you verilog
 object memblock extends App {
-  chisel3.Driver.execute(args, () => new memblock(n=16, memsize=scala.math.pow(2,13).toInt ))
+  val proto=new iofifosigs(n=16, users=4)
+  chisel3.Driver.execute(args, () => new memblock(proto, memsize=scala.math.pow(2,13).toInt ))
 }
 
 
